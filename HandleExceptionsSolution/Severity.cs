67b03bda13ad8902ab0a03b82e2786aca2106ae2
lasm16 <lasm16@yandex.ru>
2024-09-30 01:38:32 +0300

    Add project files.

---
+++ HandleExceptionsSolution/Severity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandleExceptionsSolution
+{
+    internal enum Severity
+    {
+        Warning,
+        Error
+    }
+}
